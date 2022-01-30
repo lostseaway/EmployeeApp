80437f76405896e900f27173ce5008df875c46fc
Thunyathon <lostseaway@me.com>
2022-01-30 23:31:28 +0700

    Initial Commit

---
+++ Employee/EmployeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeApp.swift
+//  Employee
+//
+//  Created by Thunyathon  Jaruchotrattanasakul on 30/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
